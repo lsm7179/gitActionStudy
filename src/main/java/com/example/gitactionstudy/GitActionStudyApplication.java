1e0cebb032e3743b74ff833d5c72fd6c97ddcb6d
lsm1010 <lsm7179@imsmobility.co.kr>
2022-05-04 16:57:50 +0900

    Initial commit

---
+++ src/main/java/com/example/gitactionstudy/GitActionStudyApplication.java
@@ -0,0 +1,13 @@
+package com.example.gitactionstudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitActionStudyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GitActionStudyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/gitactionstudy/GitActionStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gitactionstudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitActionStudyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
